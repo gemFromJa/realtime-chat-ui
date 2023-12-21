209a06b7e40c53ccea03d3f02fd74d7fbcca7cec
Stephan Woodhouse <stephankw16@gmail.com>
2023-12-20 23:25:20 -0500

    remove unused logic

--- hooks/useWebSocket.tsx
+++ hooks/useWebSocket.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import useWebSocket from "react-use-websocket";
 import { WS_API } from "../constants";
 
 export default function useRealtimeConnection(handleNewMessages: Function) {
-    const [room, setRoom] = useState("65831b373da0765b80fd44d0");
+    // const [room, setRoom] = useState("65831b373da0765b80fd44d0");
     const { sendJsonMessage, lastJsonMessage, lastMessage } =
         useWebSocket(WS_API);
 
